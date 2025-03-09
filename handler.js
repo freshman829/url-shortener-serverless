78c0176fab2ee77251aa4e621db8b214aeeeaa33
freshman829 <jacob.townsend.work@gmail.com>
2025-03-09 23:13:50 +0100

    fix handler for CORS

--- handler.js
+++ handler.js
@@ -15,6 +15,10 @@ module.exports.createUrl = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+    'Access-Control-Allow-Origin': 'https://main.d3s5lmvnk8532y.amplifyapp.com',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    },
     body: JSON.stringify({ shortUrl: `https://${event.headers.Host}/${shortId}` }),
   };
 };
@@ -28,11 +32,22 @@ module.exports.getUrl = async (event) => {
   }).promise();
 
   if (!result.Item) {
-    return { statusCode: 404, body: 'Not Found' };
+    return { 
+        statusCode: 404, 
+        headers: {
+            'Access-Control-Allow-Origin': 'https://main.d3s5lmvnk8532y.amplifyapp.com',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+        body: 'Not Found'
+    };
   }
 
   return {
     statusCode: 301,
-    headers: { Location: result.Item.originalUrl },
+    headers: {
+        'Location': result.Item.originalUrl,
+        'Access-Control-Allow-Origin': 'https://main.d3s5lmvnk8532y.amplifyapp.com',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
   };
 };
